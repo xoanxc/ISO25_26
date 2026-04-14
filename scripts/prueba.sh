70efab7ff621b890f670277a035e7f60951e5057
xoan.xc <61795755+xoanxc@users.noreply.github.com>
2026-04-14 20:53:51 +0200

    Update prueba.sh

--- scripts/prueba.sh
+++ scripts/prueba.sh
@@ -70,3 +70,68 @@ mensaje2=mundo
 
 mensaje_total="$mensaje1 $mensaje2"
 echo "$mensaje1 $mensaje2!"
+
+let "resultado=2+2"
+
+echo $resultado
+
+# 6 - Crea un programa que contenga dos variables llamadas numero1 y numero2 con valores numericos enteros. Calcula las operaciones aritmeticas basicas y muestra el resultado por pantalla de cada una.
+
+numero1=7
+numero2=9
+
+let suma=numero1+numero2
+
+echo $suma
+
+# 7 - Crea un programa que solicite al usuario un valor numérico entero entre el 1 y el 9. Muestra la tabla de multiplicar (del 1 al 9) de dicho valor.
+
+read -rep "Introduce un numero entre el 1 y el 9: " numero
+echo "Tabla de multiplicar del $numero:"
+for i in {1..9}; do
+    let resultado=numero*i
+    echo "$numero x $i = $resultado"
+done
+
+#8 - Crea un programa que reciba por parámetros dos valores enteros. El primero la base y el segundo la altura de un reptangulo. Muestra por pantalla el perímetro y al area del mismo.
+
+base=$1
+altura=$2
+
+area=$((base * altura))
+perimetro=$((2 * (base + altura)))
+
+#Otra forma:
+#area=($base * $altura)
+#perimetro=(2 * ($base + $altura))
+
+echo "Area del rectangulo: $area"
+echo "Perimetro del rectangulo: $perimetro"
+
+
+read -rep "Introduce un número: " numero
+
+if [ $numero -gt 0 ]
+then
+        echo "El valor es positivo"
+elif [ $numero -eq 0 ]
+then
+        echo "El valor es cero"      
+else
+        echo "El valor es negativo"
+fi
+
+case $numero in
+        1)
+                echo "El valor es 1"
+                ;;
+        2)
+                echo "El valor es 2"
+                ;;
+        3)
+                echo "El valor es 3"
+                ;;
+        *)
+                echo "Valor no valido"
+                ;;
+esac
