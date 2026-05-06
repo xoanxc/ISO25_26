0e470905b70b741d827ab44b24d67f7a6eb0bc58
xoan.xc <61795755+xoanxc@users.noreply.github.com>
2026-05-06 20:45:56 +0200

    Create Ejercicio 43.sh

---
+++ scripts/Ejercicios de BASH - La venganza/Ejercicio 43.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+# Variable de control.
+juego_terminado=0
+
+while [[ $juego_terminado == 0 ]]
+do
+    echo "Selecciona una de las siguientes opciones: piedra/papel/tijeras"
+    read -rp "> " opcion
+
+    # Validamos la entrada del usuario
+    if [[ "$opcion" != "piedra" && "$opcion" != "papel" && "$opcion" != "tijeras" ]]
+    then
+        echo "Opción no válida. Por favor, elige piedra/papel/tijeras."
+    else
+        cpu=$((RANDOM % 3))
+        
+        if [[ $cpu == 0 ]]; then
+            maquina="piedra"
+        elif [[ $cpu == 1 ]]; then
+            maquina="papel"
+        else
+            maquina="tijeras"
+        fi
+
+        echo "La máquina ha elegido: $maquina"
+
+        # Determinar el ganador
+        if [[ "$opcion" == "$maquina" ]]; then
+            echo "¡Es un empate! Intenta de nuevo."
+        elif [[ "$opcion" == "piedra" && "$maquina" == "tijeras" ]] || [[ "$opcion" == "papel" && "$maquina" == "piedra" ]] || [[ "$opcion" == "tijeras" && "$maquina" == "papel" ]]
+        then
+            echo "¡Felicidades, ganaste!"
+            # Terminamos el juego
+            juego_terminado=1
+        else
+            echo "¡Oh no, la máquina gana!"
+            # Terminamos el juego
+            juego_terminado=1 
+        fi
+    fi
+done
+
+
+
+
+
