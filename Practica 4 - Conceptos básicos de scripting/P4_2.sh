785aef366e7d910d5582621e9413528b4b14a89e
xoan.xc <61795755+xoanxc@users.noreply.github.com>
2026-05-08 22:21:48 +0200

    Update P4_2.sh

--- Practica 4 - Conceptos básicos de scripting/P4_2.sh
+++ Practica 4 - Conceptos básicos de scripting/P4_2.sh
@@ -1,5 +1,19 @@
 #!/bin/bash
 
+# Añadida a ultima hora para reducir la repetición de código.
+function ayudaParametros {
+    # Ayuda para el usuario en caso de que introduzca un parámetro no reconocido.
+    echo "Opciones disponibles:"
+    echo "  -p  Modificar permisos"
+    echo "  -ip Modificar estado de red"
+    echo "  -o  Modificar propietario"
+    echo "  -l Listar directorio"
+    echo "  -la  Listar directorio y ocultos"
+    echo "  -ll  Listar directorio con detalles"
+    exit 1 # Salimos con un código de error.
+}
+
+
 # Comprobar que el usuario que lanza el script tiene permisos de superusuario
 if [ $UID -ne 0 ]
 then 
@@ -10,7 +24,8 @@ fi
 # Comprobar si no se ha pasado ningún parámetro
 if [ $# -eq 0 ]; then
     echo "Error: Falta introducir parámetros."
-    exit 1
+    echo ""
+    ayudaParametros
 fi
 
 # Comprobación de parametros
@@ -39,12 +54,13 @@ case $1 in
                     echo "Interfaz $2 desactivada."
                 else
                     echo "Error: El estado debe ser up/down."
-                    echo "Uso correcto: $0 -ip <up/down> <interfaz>"
+                    echo "Uso correcto: $0 -ip <interfaz> <up/down>"
                     exit 1
                 fi
             else
+                # Aquí se podria haber utilizado una función, pero no lo vi tan necesario.
                 echo "Error de parámetros."
-                echo "Uso correcto: $0 -ip <up/down> <interfaz>"
+                echo "Uso correcto: $0 -ip <interfaz> <up/down>"
                 exit 1
             fi
             ;;
@@ -100,14 +116,6 @@ case $1 in
             ;;
         *)
             # Si introducen algo que no existe en el case mostramos las opciones
-            echo "Opción $1 no reconocida."
-            echo "Opciones disponibles:"
-            echo "  -p  Modificar permisos"
-            echo "  -ip Modificar estado de red"
-            echo "  -o  Modificar propietario"
-            echo "  -l Listar directorio"
-            echo "  -la  Listar directorio y ocultos"
-            echo "  -ll  Listar directorio con detalles"
-            exit 1
+            ayudaParametros
             ;;
 esac
